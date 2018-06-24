86b5eefe82c55f4abe339c5155925514a048c218
ZigaSajovic <ziga.sajovic@gmail.com>
2018-06-25 01:31:46 +0200

    empirical_std

--- truncDist.py
+++ truncDist.py
@@ -1,33 +1,34 @@
+import tensorflow as tf
+
 class TruncDist:
 #
-  def __init__(self,dist,left,right, N_points=1000):
+  def __init__(self,dist,left,right, n_points=1000):
     self.left=left
     self.right=right
     self.lft=dist.cdf(left)
     self.rght=dist.cdf(right)
     l_shape=left.shape
-    self.yaxis=tf.reshape(tf.map_fn(lambda lft: tf.linspace(lft,right,N_points), left.reshape(-1)),tf.concat([[N_points],l_shape],axis=0))
+    self.yaxis=tf.reshape(tf.map_fn(lambda lft: tf.linspace(lft,right,n_points), left.reshape(-1)),tf.concat([[N_points],l_shape],axis=0))
     self.xaxis=dist.cdf(self.yaxis)
     self.dist=dist
     self.batch_shape=dist.batch_shape
 #
-  def sample(self,M=()):
-    if M is ():
-      M=(1)
-    M=tf.convert_to_tensor(M)
-    if M.shape.ndims == 0:
-      M=tf.expand_dims(M,0)
-    M_shape=M
-    M=tf.reduce_prod(tf.reshape(M,[-1]))
-    print(sess.run(M))
+  def sample(self,sample_shape=()):
+    if sample_shape is ():
+      sample_shape=(1)
+    sample_shape=tf.convert_to_tensor(sample_shape)
+    if sample_shape.shape.ndims == 0:
+      sample_shape=tf.expand_dims(sample_shape,0)
+    sample_shape_original=sample_shape
+    sample_shape=tf.reduce_prod(tf.reshape(sample_shape,[-1]))
   #
-    to_sample=tf.concat(([M],tf.shape(self.xaxis)[1:]),axis=0)
+    to_sample=tf.concat(([sample_shape],tf.shape(self.xaxis)[1:]),axis=0)
     query=self.lft+tf.contrib.distributions.Uniform().sample(to_sample)*(self.rght-self.lft)
   #
     l_pad=tf.expand_dims(tf.minimum(query - 1, self.xaxis[0]),axis=0)
     r_pad=tf.expand_dims(tf.maximum(query + 1, self.xaxis[-1]),axis=0)
   #
-    to_tile_x=tf.concat([[1],[M],tf.ones(tf.size(tf.shape(self.xaxis)[1:]),dtype=tf.int32)],axis=0)
+    to_tile_x=tf.concat([[1],[sample_shape],tf.ones(tf.size(tf.shape(self.xaxis)[1:]),dtype=tf.int32)],axis=0)
     x_pad=tf.tile(tf.expand_dims(self.xaxis,1),to_tile_x)
     xaxis_pad = tf.concat([l_pad,x_pad,r_pad],axis=0)
   #
@@ -57,9 +58,7 @@ class TruncDist:
   #
     alpha = (tf.reshape(query,shape[:-1]) - tf.reshape(xps,shape[:-1])) / tf.reshape(xps1 - xps,shape[:-1])
     res = alpha * tf.reshape(yps1,shape[:-1]) + (1 - alpha) * tf.reshape(yps,shape[:-1])
-    print(sess.run(tf.shape(res)))
-    print(sess.run(M_shape))
-    return tf.squeeze(tf.reshape(res,tf.concat([M_shape,tf.shape(res)[1:]],axis=0)))
+    return tf.squeeze(tf.reshape(res,tf.concat([sample_shape_original,tf.shape(res)[1:]],axis=0)))
 #
   def cdf(self,X):
     X=tf.maximum(tf.minimum(X,self.right),self.left)
@@ -82,3 +81,6 @@ class TruncDist:
 #
   def empirical_var(self, ddof=1, n_samples=1000):
     return tf.reduce_sum(self.sample(n_samples),axis=0)/(n_samples-ddof)
+#
+  def empirical_std(self, *args, **kwargs):
+    return tf.sqrt(self.empirical_var(*args, **kwargs))
